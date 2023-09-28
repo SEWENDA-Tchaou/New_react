6f2d062dc6731c756a0d6d1d779dd2801fa5b5b7
sewenda tchaou <sewendatchaou@gmail.com>
2023-09-28 16:17:14 -0700

    add svg img again

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import k from './assets/img/k.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -14,6 +15,9 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="" target="_blank">
+          <img src={k} className="logo react" alt="React logo" />
         </a>
       </div>
       <img src="../src/assets/img/10k.svg" alt="" width={100} height={100} />
