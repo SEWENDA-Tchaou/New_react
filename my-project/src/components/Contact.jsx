0aeb421ddab9f1febf43cb99466aec91d5ee2a05
sewenda tchaou <sewendatchaou@gmail.com>
2023-10-10 07:49:08 -0700

    add content on Contact

--- my-project/src/components/Contact.jsx
+++ my-project/src/components/Contact.jsx
@@ -1,7 +1,17 @@
-
+import Navbar from '../components/Navbar'
 function Contact() {
   return (
-    <div>Contact</div>
+    <>
+         <Navbar />
+    <div className="bg-black h-[100vh] py-20 text-white ">
+       <div>
+        <h1 className="w-[246px] text-blue-600  text-2xl m-14 font-bold border-b-2 border-b-green-400">
+            Je suis joingnable
+        </h1>
+        
+       </div>
+    </div>
+    </>
   )
 }
 
