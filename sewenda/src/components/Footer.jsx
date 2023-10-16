39a0b783f2d7a534dda41ded318262239d0d8d93
sewenda tchaou <sewendatchaou@gmail.com>
2023-10-16 08:55:45 -0700

    footer add linkdin routing

--- sewenda/src/components/Footer.jsx
+++ sewenda/src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className=" absolute bottom-0  z-50">
         <div className="w-1/2 flex">
-          <a href=""><img src={linkedin} className='rounded-full ml-20 my-1'  width={40} alt="" /></a>
+          <a href="https://www.linkedin.com/in/sewenda-tchaou-7a0b1a238/"target='_blank' rel="noreferrer" className=" mx-8 rounded-full ml-20 my-1"><img src={linkedin} className='rounded-full ml-20 my-1'  width={40} alt="" /></a>
           <a href=""><img src={github} className='rounded-full my-1' width={40} style={{height:40, marginLeft: 50}} alt="" /></a>
         </div>
     </div>
